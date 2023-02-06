0e4141b7e7543187ca4e2f7ae2f1f6563f3d8359
Julia <nebo.julia@gmail.com>
2023-02-06 14:12:38 +0300

    working on burger drop navbar js

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-const burger = document.querySelector('.header__burger');
-const menu = document.querySelector('.nav')
-burger.addEventListener('click', () => {
-    burger.classList.toggle('active')
-    menu.classList.toggle('active')
-})
+"use strict"
+
+window.onload = function() {
+
+const menu = document.querySelector(".nav");
+const hamburger= document.querySelector(".header__burger");
+const closeIcon= document.querySelector(".header__closed-img");
+const menuIcon = document.querySelector(".header__burger-img");
+
+function toggleMenu() {
+    if (menu.classList.contains("show-menu")) {
+        menu.classList.remove("show-menu");
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block";
+    } else {
+        menu.classList.add("show-menu");
+        closeIcon.style.display = "block";
+        menuIcon.style.display = "none";
+    }
+}
+
+    hamburger.addEventListener("click", toggleMenu);
+}
