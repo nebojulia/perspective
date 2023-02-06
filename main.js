b787f9e2cd159bd228f4a251c57fa97b7a45c552
Julia <nebo.julia@gmail.com>
2023-02-06 16:30:59 +0300

    fix burger drop menu js

--- main.js
+++ main.js
@@ -10,10 +10,12 @@ const menuIcon = document.querySelector(".header__burger-img");
 function toggleMenu() {
     if (menu.classList.contains("show-menu")) {
         menu.classList.remove("show-menu");
+        menu.style.opacity = 0;
         closeIcon.style.display = "none";
         menuIcon.style.display = "block";
     } else {
         menu.classList.add("show-menu");
+        menu.style.opacity = 1;
         closeIcon.style.display = "block";
         menuIcon.style.display = "none";
     }
